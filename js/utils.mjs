0c322864cd4f28c5ee4dfe47928f6f3e28ec7538
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-02-09 23:03:10 -0700

    Refactor loadHeaderFooter and loadTemplate functions: improve logging and error handling; return error message in loadTemplate

--- js/utils.mjs
+++ js/utils.mjs
@@ -1,33 +1,32 @@
 export async function loadHeaderFooter() {
-    const headerElement = document.querySelector("nav");
-    const footerElement = document.querySelector("footer");
-    const headerTemplate = await loadTemplate("/templates/header.html");
-    const footerTemplate = await loadTemplate("/templates/footer.html");
-
-    renderWithTemplate(headerElement, headerTemplate);
-    renderWithTemplate(footerElement, footerTemplate);
+  const headerElement = document.querySelector("nav");
+  const footerElement = document.querySelector("footer");
+  const headerTemplate = await loadTemplate("/templates/header.html");
+  const footerTemplate = await loadTemplate("/templates/footer.html");
+  console.log(headerTemplate);
+  console.log(footerTemplate);
+  renderWithTemplate(headerElement, headerTemplate);
+  renderWithTemplate(footerElement, footerTemplate);
 }
 
 export async function loadCards() {
-    const cardsElement = document.querySelector(".shelf");
-    const cardsTemplate = await loadTemplate("/templates/card.html");
+  const cardsElement = document.querySelector(".shelf");
+  const cardsTemplate = await loadTemplate("/templates/card.html");
 
-    renderWithTemplate(cardsElement, cardsTemplate);
+  renderWithTemplate(cardsElement, cardsTemplate);
 }
 
 export async function loadTemplate(templatePath) {
-    try {
-
-        const html = await fetch(templatePath);
-        const template = await html.text();
-        return template;
-    } catch (error) {
-        console.error(`Error fetching template: ${templatePath}`, error);
-        return "";
-    }
+  try {
+    const html = await fetch(templatePath);
+    const template = await html.text();
+    return template;
+  } catch (error) {
+    console.error(`Error fetching template: ${templatePath}`, error);
+    return "<p> error loading template </p>";
+  }
 }
 
 export function renderWithTemplate(element, template) {
-    element.insertAdjacentHTML("afterbegin", template);
+  element.insertAdjacentHTML("afterbegin", template);
 }
-
