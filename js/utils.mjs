547c28e7c4e77f0c647c4a9af973c764c1781d6c
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-02-11 08:00:04 -0700

    Update navigation links in header template for relative paths

--- js/utils.mjs
+++ js/utils.mjs
@@ -1,16 +1,14 @@
 export async function loadHeaderFooter() {
   const headerElement = document.querySelector("nav");
   const headerTemplate =   `<nav>
-                                <a href="/index.html">home</a>
-                                <a href="/bookShelf/index.html">books</a>
-                                <a href="/movieShelf/index.html">movies</a>
-                                <a href="/contact.html">contact</a>
+                                <a href=".index.html">home</a>
+                                <a href="./bookShelf/index.html">books</a>
+                                <a href="./movieShelf/index.html">movies</a>
+                                <a href="./contact.html">contact</a>
                             </nav>
 `;
   const footerElement = document.querySelector("footer");
   const footerTemplate = `<p>WDD330 Final Project &copy; Jacob Humphreys 2025</p>`;
-  console.log(headerTemplate);
-  console.log(footerTemplate);
   renderWithTemplate(headerElement, headerTemplate);
   renderWithTemplate(footerElement, footerTemplate);
 }
