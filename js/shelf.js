2d6f771a201272529379cbe8347f00fecfde29b8
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-02-11 14:06:35 -0700

    Remove unused card, header, and footer templates; inline card template in shelf.js for improved performance and maintainability

--- js/shelf.js
+++ js/shelf.js
@@ -8,26 +8,34 @@ import Cards from "./Cards.mjs";
 
 loadHeaderFooter();
 
-
 async function getResponse(type, key, query) {
   const books = new Cards(type, key, query);
   await books.init();
   const topFive = books.topFive;
   const cardsElement = document.querySelector(".shelf");
-  const cardsTemplate = await loadTemplate("/templates/card.html");
+  const cardsTemplate = `<div class="card">
+  <h3 class="title">Title 1</h3>
+  <div class="description">
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia rerum
+      assumenda provident consequuntur odio.
+    </p>
+  </div>
+</div>
+`;
   await loadCards(topFive, cardsElement, cardsTemplate);
 }
 
 async function loadCards(topFive, cardsElement, cardsTemplate) {
-  for (const book of topFive) {
+  for (const _book of topFive) {
     renderWithTemplate(cardsElement, cardsTemplate);
   }
 }
 
-document.querySelector('.searchButton').addEventListener('click', () => {
-  const type = document.querySelector('.searchButton').parentElement.id;
+document.querySelector(".searchButton").addEventListener("click", () => {
+  const type = document.querySelector(".searchButton").parentElement.id;
   console.log(type);
-  const key = document.querySelector('.keySelect').value;
-  const query = document.querySelector('#searchbox').value;
+  const key = document.querySelector(".keySelect").value;
+  const query = document.querySelector("#searchbox").value;
   getResponse(type, key, query);
 });
